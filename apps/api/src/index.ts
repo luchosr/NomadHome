ae11997c6bc46dc5808d50b0e8650e49f1451e30
Luciano Ramello <luchosr@gmail.com>
2026-07-09 17:01:44 +0200

    fix(index): ensure PORT is correctly parsed as a number and specify host in app.listen

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,9 @@
 import { createApp } from "./app.js";
 
 // const PORT = Number(process.env.PORT ?? 3000);
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT || 3000);
 const app = createApp();
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.info(`[api] NomadHome API listening  on port ${PORT}`);
 });
